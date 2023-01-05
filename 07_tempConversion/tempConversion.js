d1f0e9dead9ec02231fedc8f7174f454789ed9d3
Sameer Reza <sameereza@gmail.com>
2023-01-05 10:27:42 +0800

    finishing exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitInput) {
+  //convert fahrenheit to celsius
+  let celsius = (fahrenheitInput - 32) * 5/9;
+  //round to 1 decimal
+  return Math.round(celsius * 10) / 10; //could also use toFixed(1) and then convert to number
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusInput) {
+  //convert celsius to fahrenheit
+  let fahrenheit = (celsiusInput * 9/5) + 32;
+  //round to 1 decimal
+  return Math.round(fahrenheit * 10) / 10; //could also use toFixed(1) and then convert to number
 };
 
 // Do not edit below this line
