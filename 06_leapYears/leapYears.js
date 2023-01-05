3e71480a76f814004afe69ecb3d45c4fb68b208b
Sameer Reza <sameereza@gmail.com>
2023-01-05 10:12:30 +0800

    finishing exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    //a leap year must be divisible by 4
+    //a leap year must not be divisible by 100 unless it is also divisible by 400
+    if (inputYear % 4 === 0) {
+        if (inputYear % 100 === 0) {
+            if (inputYear % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
