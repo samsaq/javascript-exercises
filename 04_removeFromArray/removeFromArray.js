636c4e13a2c1c12a48b55b9911eedc83a3ac4b2e
Sameer Reza <sameereza@gmail.com>
2023-01-05 09:48:24 +0800

    finishing exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+    //use the spread operator to get the arguments into an array
+    const removeValues = args;
+    //loop through the array and remove the values via splice to prevent holes
+    for (let item = 0; item < inputArray.length; item++) {
+        if (removeValues.includes(inputArray[item])) {
+            inputArray.splice(item, 1);
+            item--; //decrement the item to account for the splice
+        }
+    }
+    //return the array
+    return inputArray;
 };
 
 // Do not edit below this line
