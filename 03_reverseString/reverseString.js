bfddb63a1a53c5ca62be23504a702f91bfac4e53
Sameer Reza <sameereza@gmail.com>
2023-01-05 08:30:04 +0800

    finished 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //reverse a string given as an argument & return the reversed string
+    //split the string into an array of characters
+    const stringArray = string.split('');
+    //reverse the array
+    const reversedArray = stringArray.reverse();
+    //join the array into a string
+    const reversedString = reversedArray.join('');
+    //return the string
+    return reversedString;
 };
 
 // Do not edit below this line
