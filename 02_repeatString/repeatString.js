0e25ba2e2d019c418ea6bb219dc4fd0d2301eaa7
Sameer Reza <sameereza@gmail.com>
2023-01-05 08:15:36 +0800

    finishing the 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let output = "";
+    //if num is negative, return error
+    if(num < 0)
+    return "ERROR";
 
+    //loop through num and add string to output until the num of repeated strings is reached
+    for(let i = 0; i < num; i++)
+    {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
