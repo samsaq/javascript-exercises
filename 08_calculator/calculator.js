d5b399314332eec2bd8637115db3537504d2fff4
Sameer Reza <sameereza@gmail.com>
2023-01-18 20:47:52 -0700

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	let curSum = 0;
+  for(const element of inputArray)
+  {
+    curSum += element;
+  }
+  return curSum;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  let curVal = 1;
+  for(const element of inputArray)
+  {
+    curVal *= element;
+  }
+  return curVal;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 1 || a === 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
