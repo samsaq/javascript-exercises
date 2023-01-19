010ff85659b852bcce8b73d5df7e0c892681b490
Sameer Reza <sameereza@gmail.com>
2023-01-18 21:31:26 -0700

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let reversedString = "";
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        reversedString += inputString[i];
+    }
+    //convert both strings to lowercase and remove all non-alphanumeric characters
+    inputString = inputString.toLowerCase().replace(/[^a-z0-9]/g, "");
+    reversedString = reversedString.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return inputString === reversedString;
 };
+
+palindromes("racecar!");
 
 // Do not edit below this line
 module.exports = palindromes;
