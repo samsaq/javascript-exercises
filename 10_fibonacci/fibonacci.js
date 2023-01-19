bdc96b0b80d6b1ad5771b5c9d97444c38e29eda3
Sameer Reza <sameereza@gmail.com>
2023-01-18 21:53:10 -0700

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+//calculates the fibonacci number up to the x'th number and returns that value
+const fibonacci = function (x) {
+    if (x < 0) {
+        return "OOPS";
+    }
+    else if (x <= 1) {
+        return parseInt(x);
+    }
+    return fibonacci(x - 1) + fibonacci(x - 2);
 };
 
 // Do not edit below this line
